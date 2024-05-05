be1efed2f619bbcddecbd6306306b4112b5a1a1d
Joshua Uzoagulu <joshua.uzoagulu@netapps.com.ng>
2024-05-05 13:45:48 +0100

    homepage

--- src/app/create-quote/page.jsx
+++ src/app/create-quote/page.jsx
@@ -1,8 +1,163 @@
+'use client'
 import React from 'react';
+import {useState, useEffect} from 'react'
+import Select from 'react-select'
+import AsyncSelect from 'react-select/async';
+import axios from 'axios'
+
+
+
+const showType = [
+    { label: 'Movie', value: 'movie' },
+    { label: 'TV Show', value: 'tv' },
+]
+
+
+const searchMovie = async (keyword) => {
+    try {
+        const res = await axios.get(`http://localhost:5000/search?movie_type=tv&keyword=${keyword}`)
+        let movies = res.data.movies
+
+        return movies.length > 0 ? movies.map((movie) => {
+            return {
+                label: movie.movie_name,
+                value: movie.movie_id,
+                image: movie.movie_image
+            }
+        }) : []
+
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+
+const loadOptions = async (inputValue) => {
+    return searchMovie(inputValue)
+}
 
 function CreateQuote(props) {
+    const [quote, setQuote] = useState('')
+    const [movie_type, setMovieType] = useState('')
+    const [movie, setMovie] = useState('')
+    const [casts, setCasts] = useState([])
+    const [cast, setCast] = useState([])
+    const [keyword, setKeyword] = useState('')
+    const [previewImage, setPreviewImage] = useState('')
+
+    useEffect(() => {
+        searchMovie().then(res => console.log(res))
+    }, []);
+
+    const handleChange = async (newValue) => {
+        setPreviewImage(newValue.image)
+        const casts = await getCharacters(newValue.value)
+        setCasts(casts)
+        setMovie(newValue);
+    };
+
+    const getCharacters = async (movie_id) => {
+        try {
+            const res = await axios.get(`http://localhost:5000/casts?movie_type=tv&movie_id=${movie_id}`)
+            let casts = res.data.casts
+            return casts.length > 0 ? casts.map((cast) => {
+                return {
+                    label: cast.cast_name,
+                    value: cast.cast_id,
+                }
+            }) : []
+        }catch (e) {
+            console.log(e.response)
+        }
+    }
+
+    const submitQuote = async (e) => {
+        e.preventDefault()
+        const data = {
+            "movie_id": movie.value,
+            "cast_id": cast.value,
+            "movie_type": movie_type.value,
+            "quote": quote
+        }
+        try {
+            const res = await axios.post('http://localhost:5000/quotes', data)
+            console.log(res)
+            window.reload()
+        }catch (e) {
+            console.log(e.response)
+        }
+
+    }
+
     return (
-        <div>create me a quote bro</div>
+        <div className="create-section">
+            <div className="my-form p-8">
+                <div className="text-2xl font-bold p-8 text-center"> Submit Quote </div>
+                <form className="max-w-sm mx-auto" onSubmit={submitQuote}>
+                    <div className="mb-5">
+                        <label htmlFor="countries"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select show
+                            type *</label>
+                        <Select
+                            value={movie_type}
+                            onChange={(val) => setMovieType(val)}
+                            options={showType}
+                        />
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="password"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Movie Name *
+                        </label>
+                        <AsyncSelect
+                            defaultOptions
+                            value={movie}
+                            onChange={handleChange}
+                            loadOptions={loadOptions}
+                            placeholder="Search"
+                            isDisabled={!movie_type}
+                        />
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="countries"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Character *
+                            </label>
+                        <Select
+                            options={casts}
+                            value={cast}
+                            onChange={(val) => setCast(val)}
+                            isDisabled={!movie || !movie.label}
+                            placeholder="Search"
+                        />
+                    </div>
+                    <div className="mb-5">
+
+                        <label htmlFor="message"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quote *
+                        </label>
+                        <textarea id="message" rows="4"
+                                  value={quote}
+                                  onChange={(val) =>  setQuote(val.target.value)}
+                                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                  placeholder="Leave a comment..."></textarea>
+
+                    </div>
+                    <button
+                        type="submit"
+                            // disabled={true}
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Submit Quote
+                    </button>
+                </form>
+
+
+            </div>
+            <div className="preview p-5">
+                <img src={previewImage} height="px" width="250px"
+                     alt="bb"/>
+                <div className="text-center p-4">{movie.label}</div>
+            </div>
+        </div>
     );
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,106 @@
 'use client'
 import {useEffect, useState} from 'react'
+import axios from 'axios'
+import {log} from "next/dist/server/typescript/utils";
 
-export default function Home() {
-    const quotes = {
-        movie_name: "Breaking Bad(2013)",
-        character_name: "Walter White",
-        quote: `Far Far away, behind the word mountains, far from the countries Vokalia and Consonatia, there live the bline
-                      texts. Separated they lin Bookmarksgrove right at the coasti`,
-        images: ['https://image.tmdb.org/t/p/original/f3hwT3dLj3Jb8ikbxs415vKlIgg.jpg', 'https://image.tmdb.org/t/p/original/9faGSFi5jam6pDWGNd0p8JcJgXQ.jpg']
+export default function  Home() {
+    // let quote = {
+    //     movie_name: "Breaking Bad(2013)",
+    //     cast_name: "Walter White",
+    //     quote: `Far Far away, behind the word mountains, far from the countries Vokalia and Consonatia, there live the bline
+    //                   texts. Separated they lin Bookmarksgrove right at the coasti`,
+    //     images: [
+    //         'https://image.tmdb.org/t/p/original/f3hwT3dLj3Jb8ikbxs415vKlIgg.jpg',
+    //         'https://image.tmdb.org/t/p/original/9faGSFi5jam6pDWGNd0p8JcJgXQ.jpg',
+    //         "https://image.tmdb.org/t/p/original/yXSzo0VU1Q1QaB7Xg5Hqe4tXXA3.jpg",
+    //         "https://image.tmdb.org/t/p/original/alb2BU2BeBZv5dgHhuzV9ZGakfZ.jpg",
+    //         "https://image.tmdb.org/t/p/original/gc8PfyTqzqltKPW3X0cIVUGmagz.jpg",
+    //         "https://image.tmdb.org/t/p/original/vFxjuhENDjEKzWXUGKmRFct15bA.jpg",
+    //         "https://image.tmdb.org/t/p/original/dFWqgL6MYHWLOs6Pev4G0TbMHSW.jpg",
+    //         "https://image.tmdb.org/t/p/original/wSztOsbdspDi0E6dWU1cxNmYDV9.jpg",
+    //         "https://image.tmdb.org/t/p/original/5WKVhTcc1cVaCsXwEUtB8lHzgm4.jpg",
+    //         "https://image.tmdb.org/t/p/original/rKUAAwWnUJSVO7Up72qjqo1myQ4.jpg",
+    //         "https://image.tmdb.org/t/p/original/6y12pMO6lLB5xytaEq9RWwjHMJi.jpg"
+    //     ],
+    //     movie_type: 'TV Show'
+    // }
+    const [quotes, setQuotes] = useState([])
+    const [quote, setQuote] = useState('')
+    const [count, setCount] = useState(0)
+    const [quoteIndex, setQuoteIndex] = useState(0)
+
+    const [image, setImage] = useState()
+    // const [image, setImage] = useState(quote.images[count])
+
+
+    const fetchQuotes = async () => {
+        try {
+            console.log('fetching....')
+            const res = await axios.get('http://localhost:5000/quotes')
+            console.log('fetched....')
+            return res.data
+        }catch (e) {
+            console.log(e.response, 'big error')
+            return []
+        }
     }
 
-    const [image, setImage] = useState(quotes.images[0])
-
     useEffect(() => {
-        const timer = setTimeout(() => setImage(quotes.images[1]), 9 * 1000);
+        fetchQuotes().then((data) => {
+            setQuotes(data)
+            setQuote(data.length > 0 ? data[0] : '')
+            console.log(data, 'after')
+        })
+        // const timer = setInterval(() => {
+        //     if(quote !== '') {
+        //         console.log('quote from here', quote)
+        //         let index = count === quote.images.length - 1 ? 0 : count + 1
+        //         setCount(index)
+        //         setImage(quote.images[count])
+        //     }
+        // }, 3000);
+        // return () => clearTimeout(timer);
+    }, []);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if(quote !== '') {
+                console.log('quote from here', quote)
+                let index = count === quote.images.length - 1 ? 0 : count + 1
+                console.log(index)
+                setCount(index)
+                setImage(quote.images[count])
+            }
+        }, 3000);
         return () => clearTimeout(timer);
-    });
+    }, [quote, image, quoteIndex]);
+
+    const getNext = () => {
+        if(quotes.length > 0) {
+            let index = quoteIndex === quotes.length - 1 ? 0 : quoteIndex + 1
+            setQuoteIndex(index)
+            setQuote(quotes[index])
+            setImage(quotes[index].images[0])
+        }
+    }
+
   return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
           <div className="quote-section">
               <div className='card'>
                   <div className='card-quote'>
-                      <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+                      <blockquote>{quote ? quote.quote : ''}
                       </blockquote>
                   </div>
                   <div className="card-details">
-                      <div className="character">{quotes.character_name}</div>
-                      <div className="movie">{quotes.movie_name}</div>
+                      <div className="character">{quote ? quote.cast_name : ''}</div>
+                      <div className="movie">{quote ? quote.movie_name : ''}</div>
                   </div>
               </div>
               <img src={image} alt="bb"/>
+          </div>
+          <div className="quote_nav">
+              <button> Prev </button>
+              <button onClick={() => getNext()}> Next </button>
           </div>
       </main>
   )
